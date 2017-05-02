bebb2ae2547e659149af4934425b2449ac5a970c
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-01 22:39:15 -0400

    Remove caching headers

--- index.js
+++ index.js
@@ -36,38 +36,43 @@ const proxy = http.createServer((req, res) => {
     body = Buffer.concat(body);
     let reqData = [];
     const cookie = getStringFromArray(headers['set-cookie']) || undefined;
-    const stored = cache.getCachedHeaderAndBody(url, cookie);
-    if (stored.header) {
+    const stored = cache.getCachedBody(url, cookie);
+    let usingCache = false;
+    if (stored.body) {
+      usingCache = true;
       console.dir(`[${new Date()}]: Using cache for request url ${url} and cookie ${cookie}`);
-      res.writeHead(stored.statusCode || 200, stored.statusMessage, stored.headers);
-      res.end(stored.body);
-    } else {
-      let responseContent = [];
-      const realUrl = urlModule.parse(url || `http://${headers.host}`);
-      console.log(`[${new Date()}]: Sending request to ${url || `http://${headers.host}`}`);
-      const realReq = http.request({
-        protocol: realUrl.protocol,
-        hostname: realUrl.hostname,
-        path: realUrl.path,
-        port: realUrl.port,
-        host: realUrl.host,
-        method,
-        headers
-      })
-      realReq.useChunkedEncodingByDefault = false;
-      realReq.on('response', (serverRes, socket, head) => {
-        res.writeHead(serverRes.statusCode, serverRes.statusMessage, serverRes.headers);
-        const serverCookie = getStringFromArray(serverRes.headers['set-cookie']) || undefined;
-        let cookieAge = serverRes.headers['expires'] ? (serverRes.headers['expires'] - new Date()) : serverRes.headers['maxAge'] ? serverRes.headers['maxAge'] + serverRes.headers['date'] - new Date() : undefined;
-        cache.putCacheHeader(url, {
-          statusCode: serverRes.statusCode,
-          statusMessage: serverRes.statusMessage,
-          headers: serverRes.headers
-        }, serverCookie, cookieAge);
-        if (!serverRes.headers["content-length"]) {
+    }
+    let responseContent = [];
+    const realUrl = urlModule.parse(url || `http://${headers.host}`);
+    console.log(`[${new Date()}]: Sending request to ${url || `http://${headers.host}`}`);
+    const realReq = http.request({
+      protocol: realUrl.protocol,
+      hostname: realUrl.hostname,
+      path: realUrl.path,
+      port: realUrl.port,
+      host: realUrl.host,
+      method,
+      headers
+    })
+    realReq.useChunkedEncodingByDefault = false;
+    realReq.on('response', (serverRes, socket, head) => {
+      res.status = serverRes.statusCode;
+      res.statusMessage = serverRes.statusMessage
+      for (headerKey in headers) {
+        res.setHeader(headerKey, headers[headerKey]);
+      }
+      // For some reason writeHead just won't work
+      // res.writeHead(serverRes.statusCode, serverRes.statusMessage, serverRes.headers);
+      const serverCookie = getStringFromArray(serverRes.headers['set-cookie']) || undefined;
+      let cookieAge = serverRes.headers['expires'] ? (serverRes.headers['expires'] - new Date()) : serverRes.headers['maxAge'] ? serverRes.headers['maxAge'] + serverRes.headers['date'] - new Date() : undefined;
+      if (!serverRes.headers["content-length"] && !usingCache) {
+        setTimeout(() => {
           res.end();
-          return;
-        }
+        }, 3000);
+      }
+      if (usingCache) {
+        res.end(stored.body);
+      } else {
         serverRes.on('data', (chunk) => {
           responseContent.push(chunk);
         }).on('end', (chunk) => {
@@ -76,17 +81,21 @@ const proxy = http.createServer((req, res) => {
           }
           responseContent = Buffer.concat(responseContent);
           cookieAge = serverRes.headers['expires'] ? (serverRes.headers['expires'] - new Date()) : serverRes.headers['maxAge'] ? serverRes.headers['maxAge'] + serverRes.headers['date'] - new Date() : undefined;
+          if (!cookie) {
+            cache.putCacheBody(url, responseContent, cookie, cookieAge);
+          }
           cache.putCacheBody(url, responseContent, serverCookie, cookieAge);
           res.end(responseContent);
         }).on('error', (err) => {
           console.error(err);
           throw err;
         });
-      }).on('error', (err) => {
-        throw err;
-      });
-      realReq.end(body);
-    }
+      }
+    }).on('error', (err) => {
+      throw err;
+    });
+    realReq.end(body);
+    
   }).on('error', (err) => {
     throw err;
   });
--- mem-cache/index.js
+++ mem-cache/index.js
@@ -1,60 +1,19 @@
-const nonCookiedHeader = {};
-const cookiedHeader = {};
 const nonCookiedBody = {};
 const cookiedBody = {};
 
 module.exports = {
-  getCachedHeaderAndBody: function(url, cookie) {
+  getCachedBody: function(url, cookie) {
     if (!url) {
       throw new Error(`Missing required argument!`);
     }
     if (!cookie) {
       return {
-        header: nonCookiedHeader[url],
         body: nonCookiedBody[url]
       };
     } else {
       return {
-        statusCode: cookiedHeader[url][cookie].statusCode,
-        statusMessage: cookiedHeader[url][cookie].statusMessage,
-        header: cookiedHeader[url][cookie].header,
         body: cookiedBody[url][cookie]
       };
-    }
-  },
-
-  putCacheHeader: function(url, content, cookie, timeout = 300000) {
-    if (!url || content == undefined) {
-      throw new Error(`Missing required argument! ${arguments[0]} ${arguments[1]} ${arguments[2]} ${arguments[3]}`);
-    }
-
-    if (!cookie) {
-      delete content['set-cookie'];
-      nonCookiedHeader[url] = content;
-      setTimeout(() => {
-        if (!delete nonCookiedHeader[url]) {
-          console.warn('Failed to remove cached record');
-        }
-      }, timeout);
-    } else {
-      if (!cookiedHeader[url]) {
-        cookiedHeader[url] = {};
-      }
-      cookiedHeader[url][cookie] = content;
-      setTimeout(() => {
-        if (cookiedHeader[url]) {
-          if (!delete cookiedHeader[url][cookie]) {
-            console.warn('Failed to remove cached record');
-          }
-          let size = 0;
-          for (let key in cookiedHeader[url]) {
-            if (obj.hasOwnProperty(key)) size++;
-          }
-          if (!size) {
-            delete cookiedHeader[url];
-          }
-        }
-      }, timeout);
     }
   },
 
